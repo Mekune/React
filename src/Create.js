f9e7ad1232800b964bb7db1b658f658775c4efb2
Mekune <nans.diaz30@gmail.com>
2024-02-02 15:35:35 +0100

    Adding blog (form & submit)

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Create() {
+  const [title, setTitle] = useState("test");
+  const [body, setBody] = useState("test body");
+  const [author, setAuthor] = useState("mekune");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  };
+
   return (
     <div>
-      <h2> add a new block </h2>
+      <h2> add a new blog </h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mekune">mekune</option>
+          <option value="mekune2">mekune2</option>
+          <option value="mekune3">mekune3</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 }
--- src/Route_Param.js
+++ src/Route_Param.js
@@ -2,19 +2,26 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import BlogDetail from "./BlogDetail.js";
 import Custom_Hook from "./Custom_Hook.js";
+import Create from "./Create.js";
 
 export default function Routers() {
   return (
     <section className="test">
-      <h2 className="title">14. Route Param</h2>
+      <h2 className="title">18. Adding blog (form & submit)</h2>
       <nav>
         <ul>
           <li>
             <Link to="/blogs">blogs</Link>
           </li>
+          <li>
+            <Link to="/create">create</Link>
+          </li>
         </ul>
       </nav>
       <Switch>
+        <Route path="/create">
+          <Create />
+        </Route>
         <Route path="/blogs">
           <Custom_Hook />
         </Route>
